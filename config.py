9f70580f1759532b2e4748caac4f56464c031b8d
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-05-15 12:18:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 23291931
 api_hash = "4b11dd648188731fb7c9bc8083e8791c"
-bot_token = "6821309649:AAHdY0yuWpFJdgk7p6PE3OSR_McGv3W8oWo"
+bot_token = "6751877582:AAFfQkMZQX-5ezH5hoZkJpN8vBdE3EN6oQY"
 auth_users = "5186250641,6594402123,5331045824,5935537622,5724356290,6436809802,7191218144"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "6594402123"
