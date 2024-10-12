da92bca1ff4086de9944ec676292301dd9ff8f02
Chintapakdumdum <48258553+chintapakdumdum22@users.noreply.github.com>
2024-10-12 19:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+# app.py
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from LuciferBanker'
-
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run()
-  
+    app.run(host='0.0.0.0', port=8080)
