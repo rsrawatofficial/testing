7a2c71bfb5fa0ec09f0b9dbd99d0b6ea8f83ca4c
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-02 17:51:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 20736921
-api_hash = "42b34442e52dc3e07b3e0783389be8cb"
-bot_token = "8015663864:AAGLRoTMXkj9Ndq4PL7oKLo0AtaYT68rxCM"
-auth_users = "1366730834"
+api_id = 27900743
+api_hash = "ebb06ea8d41420e60b29140dcee902fc"
+bot_token = "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8"
+auth_users = "7804396225"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "1366730834"
+osowner_users = "7804396225"
 owner_users = [int(num) for num in osowner_users.split(",")]
